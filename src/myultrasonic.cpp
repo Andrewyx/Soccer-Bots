fc140ab5ae1ab93475b2149a5b45f36c39ddb0f8
ZenBubble <apersonnameddorson@gmail.com>
2023-05-15 10:34:34 -0700

    stupid andrew code

--- src/myultrasonic.cpp
+++ src/myultrasonic.cpp
@@ -3,8 +3,8 @@
 
 const int leftTrigPin = 14;
 const int leftEchoPin = 27;
-const int rightTrigPin = 12;
-const int rightEchoPin = 13;
+const int rightTrigPin = 25;
+const int rightEchoPin = 33;
 float leftDistance;
 float rightDistance;
 unsigned long previousMillis = 0;
@@ -12,8 +12,8 @@ int interval = 1;
 int interval2 = 200;
 int leftTrigState = LOW;
 int rightTrigState = LOW; //state of trigPin
-int printState = LOW;
 unsigned long currentMillis;
+int leftDuration, rightDuration; 
 
 void initUltrasonic() {
   pinMode(leftTrigPin, OUTPUT); 
@@ -23,48 +23,23 @@ void initUltrasonic() {
 }
 
 void runUltrasonic() {
-	currentMillis = millis(); //time in milliseconds from which the code was started
-	if (currentMillis-previousMillis >= interval) { //check "blink without delay" code
-		previousMillis = currentMillis;
-		leftTrigState = !leftTrigState;
-		rightTrigState = !rightTrigState;
-	}
-	// printing if statement
-	if (currentMillis-previousMillis >= interval2) { //check "blink without delay" code
-		previousMillis = currentMillis;
-		if (printState == LOW){
-			(printState = HIGH);
-		}
-		else {
-			(printState = LOW);
-		}
-	}
-	if(leftTrigState == HIGH){
+
 		digitalWrite(leftTrigPin, HIGH);
+
 		delayMicroseconds(10);
 		digitalWrite(leftTrigPin, LOW);
-		leftTrigState = LOW;
-	}
+		delayMicroseconds(2);
+		leftDuration = pulseIn(leftEchoPin,HIGH);
+		leftDistance = (leftDuration/2) / 29.1;
+		Serial.print("Left distance:");
+		Serial.println(leftDistance);
 
-	if(rightTrigState == HIGH){
 		digitalWrite(rightTrigPin, HIGH);
 		delayMicroseconds(10);
-		digitalWrite(rightTrigPin, LOW);
-		rightTrigState = LOW;
-	}
-	int leftDuration,rightDuration; 
-	leftDuration = pulseIn(leftEchoPin,HIGH);
-	rightDuration = pulseIn(rightEchoPin,HIGH);
-	leftDistance = (leftDuration/2) / 29.1;
-	rightDistance = (rightDuration/2) / 29.1;
-if (printState = HIGH){
-    Serial.print("Left distance:");
-  	Serial.println(leftDistance);
-    Serial.print("Right distance:");
-  	Serial.println(rightDistance);
-}
-
-//left ~130
-//right ~100
-
+		digitalWrite(rightTrigPin, LOW);	
+		delayMicroseconds(2);
+		rightDuration = pulseIn(rightEchoPin,HIGH);
+		rightDistance = (rightDuration/2) / 29.1;	
+		Serial.print("Right distance:");
+		Serial.println(rightDistance);	
 }
