54ac307e7a9816845b5fc5f89c22614b0b6e4cea
ZenBubble <apersonnameddorson@gmail.com>
2023-05-16 10:02:50 -0700

    deleted some stuff

--- src/myultrasonic.cpp
+++ src/myultrasonic.cpp
@@ -15,11 +15,41 @@ int rightTrigState = LOW; //state of trigPin
 unsigned long currentMillis;
 int leftDuration, rightDuration; 
 
+
+double leftKalman(double U){
+  static const double F = 40;
+  static const double D = 1.00;
+  static double S = 10;
+  static double L = 0;
+  static double V_hat = 0;
+  static double bozo = 0;
+  bozo = L*D/(D*L*D+F);
+  V_hat += + bozo*(U-D*V_hat);
+  L = (1-bozo*D)*L+S;
+  return V_hat;
+}
+double rightKalman(double U){
+  static const double R = 40;
+  static const double H = 1.00;
+  static double Q = 10;
+  static double P = 0;
+  static double U_hat = 0;
+  static double K = 0;
+  K = P*H/(H*P*H+R);
+  U_hat += + K*(U-H*U_hat);
+  P = (1-K*H)*P+Q;
+  return U_hat;
+}
+
 void initUltrasonic() {
   pinMode(leftTrigPin, OUTPUT); 
   pinMode(leftEchoPin, INPUT); 
   pinMode(rightTrigPin, OUTPUT); 
   pinMode(rightEchoPin, INPUT); 
+}
+
+double midPointVal(double val1, double val2){
+	return (val1 + val2)/2;
 }
 
 void runUltrasonic() {
@@ -32,7 +62,7 @@ void runUltrasonic() {
 		leftDuration = pulseIn(leftEchoPin,HIGH);
 		leftDistance = (leftDuration/2) / 29.1;
 		Serial.print("Left distance:");
-		Serial.println(leftDistance);
+		Serial.println(leftKalman(leftDistance));
 
 		digitalWrite(rightTrigPin, HIGH);
 		delayMicroseconds(10);
@@ -41,5 +71,11 @@ void runUltrasonic() {
 		rightDuration = pulseIn(rightEchoPin,HIGH);
 		rightDistance = (rightDuration/2) / 29.1;	
 		Serial.print("Right distance:");
-		Serial.println(rightDistance);	
+		Serial.println(rightKalman(rightDistance));	
+
+		delay(500);
+
+
+
+
 }
